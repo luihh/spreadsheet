f33dae1becdff00917464e60f30f92fe87d68884
Luihh <luihh@proton.me>
2024-12-15 18:07:08 -0500

    add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,10 @@ import tailwind from '@astrojs/tailwind';
 
 import icon from 'astro-icon';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind(), icon()],
+  adapter: cloudflare()
 });
