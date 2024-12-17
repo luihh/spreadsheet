bd8deaf54d29026f48a9bd047384acbecb6a9466
Luihh <luihh@proton.me>
2024-12-17 16:25:05 -0500

    add output: server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import icon from 'astro-icon';
+import icon from "astro-icon";
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  output: "server",
 });
+
